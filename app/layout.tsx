f37ac61b1dbdcbbf37b78d22a056eeaeac093ff8
Eren Hasan KASAP <eren.hasan.kasap@estiem.org>
2026-02-22 11:20:39 +0300

    fix: global website title uploaded

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import "./globals.css";
 import Footer from "../components/Footer"
 import 'katex/dist/katex.min.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Eren Hasan Kasap Portfolio',
+  description: 'Mathematical Engineering Student & Software Developer Portfolio',
+};
 
 export default function RootLayout({
   children,
